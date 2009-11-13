4cafba844775a56821ce75e295e9b1698f59e07b
Lambert CLARA <lambert.clara@yahoo.fr>
2009-11-13 21:26:33 +0100

    Output command line inputs

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import
+(
+    "flag"; // command line option parser
+    "fmt";  // package implementing formatted I/O.
+    "os";   // allow access to Stdout
+)
 
-func main() {
-    fmt.Printf("hello, world\n")
+func main()
+{
+    flag.Parse(); // scans the arg list and sets up flags ??
+    //var pec string = "";
+    if flag.NArg() > 0
+    {
+        fmt.Printf("Args given :\n");
+    }
+    else
+    {
+        fmt.Printf("No args\n")
+    }
+
+    for i := 0; i < flag.NArg(); i++
+    {
+        os.Stdout.WriteString(flag.Arg(i) + "\n")
+    }
 }
 

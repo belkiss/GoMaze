c957494ede7ef2975682740cff76b0cbeb75dea1
Lambert CLARA <lambert.clara@yahoo.fr>
2009-11-13 22:11:34 +0100

    Add SMazeCell struct to handle doors of cells;

--- main.go
+++ main.go
@@ -7,10 +7,57 @@ import
     "os";   // allow access to Stdout
 )
 
+type SMazeCell struct
+{
+    north_open bool;
+    west_open  bool;
+    south_open bool;
+    east_open  bool;
+};
+
+func (inpMazeCell *SMazeCell) display()
+{
+    if inpMazeCell.north_open == true
+    {
+        os.Stdout.WriteString("Nop\n")
+    }
+    else
+    {
+        os.Stdout.WriteString("Ncl\n")
+    }
+
+    if inpMazeCell.west_open == true
+    {
+        os.Stdout.WriteString("Wop\n")
+    }
+    else
+    {
+        os.Stdout.WriteString("Wcl\n")
+    }
+
+    if inpMazeCell.south_open == true
+    {
+        os.Stdout.WriteString("Sop\n")
+    }
+    else
+    {
+        os.Stdout.WriteString("Scl\n")
+    }
+
+    if inpMazeCell.east_open == true
+    {
+        os.Stdout.WriteString("Eop\n")
+    }
+    else
+    {
+        os.Stdout.WriteString("Ecl\n")
+    }
+}
+
 func main()
 {
-    flag.Parse(); // scans the arg list and sets up flags ??
-    //var pec string = "";
+    flag.Parse(); // scans the arg list and sets up flags
+
     if flag.NArg() > 0
     {
         fmt.Printf("Args given :\n");
@@ -24,5 +71,9 @@ func main()
     {
         os.Stdout.WriteString(flag.Arg(i) + "\n")
     }
+
+    var caze *SMazeCell = &SMazeCell{false, false, false, false};
+
+    caze.display();
 }
 

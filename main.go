39d0a790785365de0ea83070b293e6951a0ce882
Lambert CLARA <lambert.clara@yahoo.fr>
2009-11-22 12:49:19 +0100

    * Open a x11 window and add method to draw a cell   with or without walls

--- main.go
+++ main.go
@@ -5,19 +5,25 @@ import
     "flag"; // command line option parser
     "fmt";  // package implementing formatted I/O.
     "os";   // allow access to Stdout
+    "exp/draw";     // allow access to draw primitives
+    "exp/draw/x11"; // allow x11 dialog init
 )
+
 
 type SMazeCell struct
 {
-    north_open bool;
-    west_open  bool;
-    south_open bool;
-    east_open  bool;
+    northOpen,
+    westOpen,
+    southOpen,
+    eastOpen  bool;
 };
 
-func (inpMazeCell *SMazeCell) display()
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+func (inpMazeCell *SMazeCell) displayText()
 {
-    if inpMazeCell.north_open == true
+    if inpMazeCell.northOpen
     {
         os.Stdout.WriteString("Nop\n")
     }
@@ -26,7 +32,7 @@ func (inpMazeCell *SMazeCell) display()
         os.Stdout.WriteString("Ncl\n")
     }
 
-    if inpMazeCell.west_open == true
+    if inpMazeCell.westOpen
     {
         os.Stdout.WriteString("Wop\n")
     }
@@ -35,7 +41,7 @@ func (inpMazeCell *SMazeCell) display()
         os.Stdout.WriteString("Wcl\n")
     }
 
-    if inpMazeCell.south_open == true
+    if inpMazeCell.southOpen
     {
         os.Stdout.WriteString("Sop\n")
     }
@@ -44,7 +50,7 @@ func (inpMazeCell *SMazeCell) display()
         os.Stdout.WriteString("Scl\n")
     }
 
-    if inpMazeCell.east_open == true
+    if inpMazeCell.eastOpen
     {
         os.Stdout.WriteString("Eop\n")
     }
@@ -54,6 +60,117 @@ func (inpMazeCell *SMazeCell) display()
     }
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+func (inpMazeCell *SMazeCell) draw(inContext draw.Context, inSquareSize int)
+{
+    vl_screen := inContext.Screen();
+
+    upXLeft, upYLeft := 100, 100;
+    squareSize := inSquareSize;
+
+    fullCell := draw.Rect(upXLeft, upYLeft, squareSize + upXLeft, squareSize + upYLeft);
+    draw.Draw(vl_screen, fullCell, draw.Black, nil, draw.ZP);
+
+    // inset set the color of the inside
+    draw.Draw(vl_screen, fullCell.Inset(1), draw.White, nil, draw.ZP);
+
+    //hide a wall with a rectangle... POOR SOLUTION !!
+    if inpMazeCell.northOpen
+    {
+        // up wall
+        hiderup := draw.Rect(upXLeft, upYLeft,
+                             upXLeft + squareSize, upYLeft + 1);
+        draw.Draw(vl_screen, hiderup, draw.White, nil, draw.ZP);
+    }
+    else
+    {
+        // do not hide
+    }
+
+    if inpMazeCell.westOpen
+    {
+        // right wall
+        hiderright := draw.Rect(upXLeft + squareSize - 1, upYLeft,
+                                upXLeft + squareSize, upYLeft + squareSize);
+        draw.Draw(vl_screen, hiderright, draw.White, nil, draw.ZP);
+    }
+    else
+    {
+        // do not hide
+    }
+
+    if inpMazeCell.southOpen
+    {
+        // down wall
+        hiderdown := draw.Rect(upXLeft, upYLeft + squareSize - 1,
+                              upXLeft + squareSize, upYLeft + squareSize);
+        draw.Draw(vl_screen, hiderdown, draw.White, nil, draw.ZP);
+    }
+    else
+    {
+        // do not hide
+    }
+
+    if inpMazeCell.eastOpen
+    {
+        // left wall
+        hiderleft := draw.Rect(upXLeft, upYLeft,
+                               upXLeft + 1, upYLeft + squareSize);
+        draw.Draw(vl_screen, hiderleft, draw.White, nil, draw.ZP);
+    }
+    else
+    {
+        // do not hide
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+func app(inContext draw.Context)
+{
+    vl_screen := inContext.Screen();
+
+    screenr := draw.Rect(0, 0, vl_screen.Width(), vl_screen.Height());
+    draw.Draw(vl_screen, screenr, draw.White, nil, draw.ZP);
+
+    squareSize := 30;
+
+    var cell *SMazeCell = new(SMazeCell);
+    cell.draw(inContext, squareSize);
+
+    inContext.FlushImage();
+
+    fmt.Printf("Press the any key to exit.\n");
+    for
+    {
+        select
+        {
+            case r := <- inContext.KeyboardChan():
+                switch r
+                {
+                    case 'q', 'Q', 0x04, 0x7F, 32 :
+                        fmt.Printf("Exiting because of keyboard event %d\n", r);
+                        os.Exit(0);
+                    default :
+                        fmt.Printf("Exiting because of keyboard event %d\n", r);
+                }
+            case <- inContext.MouseChan():
+                // No-op.
+            case <- inContext.ResizeChan():
+                // No-op.
+            case <- inContext.QuitChan():
+                fmt.Printf("Exiting because of QuitChan\n");
+                return;
+        }
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 func main()
 {
     flag.Parse(); // scans the arg list and sets up flags
@@ -72,8 +189,19 @@ func main()
         os.Stdout.WriteString(flag.Arg(i) + "\n")
     }
 
-    var caze *SMazeCell = &SMazeCell{false, false, false, false};
+//     var cell *SMazeCell = new(SMazeCell);
+// //     var cell *SMazeCell = &SMazeCell{false, false, false, false};
+// 
+//     cell.displayText();
 
-    caze.display();
+    // window init start
+    x11Context, x11Error := x11.NewWindow();
+    if x11Error != nil
+    {
+        fmt.Printf("Error: %v\n", x11Error);
+        return;
+    }
+    // window init end
+    app(x11Context);
+
 }
-
